1a57151f2e5f4c3f680b4f1521ec2a86e6ef55d8
Dylan Wilfrido Guzman Quintero <dylanwguzmanq@outlook.com>
2021-12-09 11:25:02 -0800

    se agrego la ruta del api

--- src/routes/inventarios.js
+++ src/routes/inventarios.js
@@ -12,4 +12,32 @@ router.put("/update/:id", InventarioControllers.actualizarProducto);
 
 router.delete("/delete/:id", InventarioControllers.borrarProducto);
 
+console.log("");
+
+console.log("Obtener datos");
+console.log("http://192.168.2.50:3900/api/get");
+
+console.log("");
+
+console.log("buscar un articulo en especifico");
+console.log("http://192.168.2.50:3900/api/get/:id");
+
+console.log("");
+
+console.log("guardar articulo");
+console.log("http://192.168.2.50:3900/api/save");
+
+console.log("");
+
+console.log("Actualizar Articulo");
+console.log("http://192.168.2.50:3900/api/update/:id");
+
+console.log("");
+
+console.log("Borrar Articulo");
+console.log("http://192.168.2.50:3900/api/delete/:id");
+console.log("");
+console.log("");
+
+
 module.exports = router;
